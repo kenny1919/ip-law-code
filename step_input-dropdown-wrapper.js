8db5aee3a2a2aad23d8e449b04bc18e63a22cd45
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-08-30 00:06:26 +0100

    Create step_input-dropdown-wrapper.js

---
+++ step_input-dropdown-wrapper.js
@@ -0,0 +1,7 @@
+// Get all elements with the class 'step_input-dropdown-wrapper'
+const elements = document.querySelectorAll('.step_input-dropdown-wrapper');
+
+// Iterate over each element and add the 'is-hidden' class
+elements.forEach((element) => {
+element.classList.add('is-hidden');
+});
